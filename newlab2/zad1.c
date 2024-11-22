cf4d82717bc0e4b6dc35960ec737fae8929888de
SkullSkif <124018036+SkullSkif@users.noreply.github.com>
2024-11-23 04:17:57 +0700

    Add files via upload

---
+++ newlab2/zad1.c
@@ -0,0 +1,56 @@
+#include <math.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const double eps = 1E-6;
+const int n0 = 100;
+const double a = 0.1, b = 1.0;
+
+double getrand()
+{
+    return (double)rand() / RAND_MAX;
+}
+
+double func(double x)
+{
+    return (log(1 + x)) / (x);
+}
+
+int main(int argc, char** argv)
+{
+
+    int commsize, rank;
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int n = n0, k;
+    double sq[2], delta = 1;
+    double start_time, time;
+    if (rank == 0)
+        start_time = MPI_Wtime();
+        
+    for (k = 0; delta > eps; n *= 2, k ^= 1) {
+        // int points_per_proc = n / commsize;
+        // int lb = rank * points_per_proc;
+        // int ub = (rank == commsize - 1) ? (n - 1) : (lb + points_per_proc - 1);
+        double h = (b - a) / n;
+        double s = 0.0;
+        for (int i = rank; i <= n; i += commsize)
+            s += func(a + h * (i + 0.5));
+        MPI_Allreduce(&s, &sq[k], 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+        sq[k] *= h;
+        if (n > n0)
+            delta = fabs(sq[k] - sq[k ^ 1]) / 3.0;
+    } 
+    if (rank == 0) {
+        time = MPI_Wtime() - start_time;
+        printf("Result Pi: %.12f; Runge rule: EPS %e, n %d\n",
+               sq[k] * sq[k],
+               eps,
+               n / 2);
+        printf("time: %lfs\n", time);
+    }
+    MPI_Finalize();
+    return 0;
+}
---
+++ newlab2/zad2.c
@@ -0,0 +1,50 @@
+#include <math.h>
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+const double PI = 3.14159265358979323846;
+const int n = 10000000;
+
+double getrand()
+{
+    return (double)rand() / RAND_MAX;
+}
+
+double func(double x, double y)
+{
+    return exp(pow((x + y), 2));
+}
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+    int rank, commsize;
+    double start_time, time;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &commsize);
+    if (rank == 0)
+        start_time = MPI_Wtime();    
+    srand(rank);
+    int in = 0;
+    double s = 0;
+    for (int i = rank; i < n; i += commsize) {
+        double x = getrand();           // x in [0, 1]
+        double y = getrand() * (1 - x); // y in [0, 1 - x]
+        if (y <= (1 - x)) {
+            in++;
+            s += func(x, y);
+        }
+    }
+    int gin = 0;
+    MPI_Reduce(&in, &gin, 1, MPI_INT, MPI_SUM, 0, MPI_COMM_WORLD);
+    double gsum = 0.0;
+    MPI_Reduce(&s, &gsum, 1, MPI_DOUBLE, MPI_SUM, 0, MPI_COMM_WORLD);
+    if (rank == 0) {
+        double v = 0.5 * gin / n;
+        double res = v * gsum / gin;
+        printf("Result: %.12f, n %d\n", res, n);
+    }
+    MPI_Finalize();
+    return 0;
+}
